a393b939148d460fd1e7cae88f14f2ec32e856b4
Leo Galey IV <leog4@KCSF.local>
2017-11-30 09:21:05 -0600

    final commit including comments

--- liri.js
+++ liri.js
@@ -1,31 +1,32 @@
-var twitter = require('twitter');
-var request = require('request');
-var keys = require('./keys.js');
-var Spotify = require('node-spotify-api');
-var spotifyKeys = {
+var twitter = require('twitter');  //twitter module
+var request = require('request');  //request module
+var keys = require('./keys.js');   //twitter key
+var Spotify = require('node-spotify-api'); //spotify module
+var fs = require("fs");  //file system module
+var spotifyKeys = {  
 	id: '6b914e9ddf3b43f7b66be77ff6dfb626',
 	secret: '4a5194bfda694ac5bdb220eb74a2a317'
 }
-var fs = require("fs");
 
-var command = process.argv[2];
 
-var input = process.argv.slice(3).join(' ');
+var command = process.argv[2];  //command argument  (spotify,twitter, etc)
+
+var input = process.argv.slice(3).join(' '); //build all arguments after command argument into a string
 
 var twitterCommand = function(){
 
-	var client = new twitter(keys);
-	client.get('search/tweets', {q: 'BootcampLeo'}, function(error, tweets, response) {
+	var client = new twitter(keys);  //pass in keys object
+	client.get('search/tweets', {q: 'BootcampLeo'}, function(error, tweets, response) {  //I'd imagine you could do this for a specifc twitter id, but search is working so I left it
    		if(error){
-   			console.log(error);
-   			return;
+   			return console.log(error);
    		}
 
-
    		for (i=0;i<tweets.statuses.length;i++){
-   			console.log('-----------------');
-   			console.log('Tweet Contents: ' + tweets.statuses[i].text);
-   			console.log('Created: ' + tweets.statuses[i].created_at);
+
+   			var logString = '\n-----------------';
+   			logString +=  '\nTweet Contents: ' + tweets.statuses[i].text;
+   			logString += '\nCreated: ' + tweets.statuses[i].created_at;
+   			log(logString);
    			
    		}
 
@@ -35,7 +36,7 @@ var twitterCommand = function(){
 
 var spotifyCommand = function(song){
 
-	var client = new Spotify(spotifyKeys);
+	var client = new Spotify(spotifyKeys);//pass in keys object
  	if(song == ''){
  		song = 'the sign ace of base';
  	}
@@ -43,20 +44,24 @@ var spotifyCommand = function(song){
   		if (err) {
     		return console.log('Error occurred: ' + err);
   		}
- 
-	var title = data.tracks.items[0].name; 
-	var album = data.tracks.items[0].album.name; 
-	var artist = data.tracks.items[0].artists[0].name; 
-	var preview = data.tracks.items[0].preview_url; 
-	if (preview == null){
-		preview = 'Not Available';
-	}
-	console.log('-----------------');
-	console.log('Title: ' + title);
-	console.log('Album: ' + album);
-	console.log('Artist: ' + artist);
-	console.log('Preview Link: ' + preview);
-
+  		//seemed like it was just returning an empty-ish response if no song found, not necessarily an error.  this handles that. 
+  		else if(data.tracks.total == 0){  
+  			return console.log('\nSong Not Found!');
+  		}
+  		else {
+		 	console.log(data);
+			var title = data.tracks.items[0].name; 
+			var album = data.tracks.items[0].album.name; 
+			var artist = data.tracks.items[0].artists[0].name; 
+			var preview = data.tracks.items[0].preview_url; 
+			if (preview == null){
+				preview = 'Not Available';
+			}
+			var logString = '\n-----------------';
+			logString += '\nTitle: ' + title + '\nAlbum: ' + album;
+			logString += '\nArtist: ' + artist + '\nPreview Link: ' + preview;
+			log(logString);
+		}
 	});
 
 
@@ -69,32 +74,35 @@ var movieCommand = function(movie){
 	}
 	var url = 'http://www.omdbapi.com/?apikey=6e987347&t=' + movie;
 	request(url, function (error, response, body) {
-	  if(error){
-	  	console.log('error:', error); // Print the error if one occurred
-	  }
-	  body = JSON.parse(body);
-	  var imdbRating = 'unknown';
-	  var rottenRating = 'unkown';
-	  var ratings = body.Ratings;
+		body = JSON.parse(body);
+		if(error){
+			console.log('error:', error); // Print the error if one occurred
+		}
+		else if(body.Response == 'False'){
+	  		console.log('\n' + body.Error);
+		}
 
-	  for(i=0; i<ratings.length;i++){
-	  	if (ratings[i].Source == 'Internet Movie Database'){
-	  		imdbRating = ratings[i].Value;
-	  	}
-	  	else if(ratings[i].Source == 'Rotten Tomatoes'){
-	  		rottenRating = ratings[i].Value;
+		else {
+			var imdbRating = 'unknown';
+			var rottenRating = 'unkown';
+			var ratings = body.Ratings;
 
-	  	}
-	  }
-	  console.log('-----------------');
-	  console.log('Title: ', body.Title); 
-	  console.log('Year: ', body.Year);
-	  console.log('IMDB Rating: ', imdbRating); 
-	  console.log('Rotten Tomatoes Rating: ', rottenRating); 
-	  console.log('Language: ', body.Language); 
-	  console.log('Plot: ', body.Plot); 
-	  console.log('Actors: ', body.Actors); 
+			for(i=0; i<ratings.length;i++){
+				if (ratings[i].Source == 'Internet Movie Database'){
+		  			imdbRating = ratings[i].Value;
+		  		}
+		  		else if(ratings[i].Source == 'Rotten Tomatoes'){
+		  			rottenRating = ratings[i].Value;
+		  		}
+		  	}
 
+			var logString = '\n-----------------';
+			logString += '\nTitle: ' +  body.Title + '\nYear: ' + body.Year + '\nIMDB Rating: ' +  imdbRating;
+			logString += '\nRotten Tomatoes Rating: ' + rottenRating + '\nLanguage: ' + body.Language;
+			logString += '\nPlot: ' + body.Plot + '\nActors: ' + body.Actors;
+			
+			log(logString);
+		}
 
 	});
 
@@ -105,13 +113,13 @@ var doWhatItSaysCommand = function(){
 		if (error) {
     		return console.log(error);
   		}
-  	var dataArr = data.split(",");
-  	commandRouting(dataArr[0],dataArr[1]);
+  	var dataArr = data.split(",");//split the contents of random.txt on the commas, result is array
+  	commandRouting(dataArr[0],dataArr[1]);  //send back to routing function with command and input arguments from .txt file
 	});
 
 }
 
-
+//determine what to do based on command string, use input as argument where necessary
 var commandRouting = function(command,input){
 	switch (command) {
 		case 'my-tweets':
@@ -127,19 +135,21 @@ var commandRouting = function(command,input){
 			doWhatItSaysCommand();
 			break;
 		default:
-			console.log('Command Not Found');
+			console.log('\nCommand Not Found');
 
 	}
 }
 
-
+//log result data to console and to log.txt file.  appendFile will create file if it doesn't exist. 
 var log = function(data){
+	console.log(data);
 	fs.appendFile('log.txt', data, function(err) {
   		if (err) {
   			console.log(err);
-  		}					
+  		}	
+
 	});
 }
 
-
+//this executes first when the app is ran
 commandRouting(command,input);
